4f5b4a216a013af9f23175e47af7b941da180c70
Tracy Byrdsong <tracy.byrdsong@pursuit.org>
2025-06-18 18:44:14 -0400

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+
+if name == "AI Director":
+    print("Hey, it's the awesome AI Director, AI Director!")
+else:
+    print(f"Hello, {name}! Nice to meet you.")
